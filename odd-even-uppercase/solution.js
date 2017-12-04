a75525c73c6ba53adfa6726c76d2c309c262722d
svivek71@yahoo.com <svivek71@yahoo.com>
2017-12-04 14:34:07 -0800

    odd-even working

--- odd-even-uppercase/solution.js
+++ odd-even-uppercase/solution.js
@@ -1,10 +1,11 @@
 function capitalize(s){
-    let oddstr = '';
-    let evenstr = '';
-    s.forEach(function(value,index,arr){
-       (index%2 == 0) ? (oddstr = oddstr + value , evenstr = evenstr + toUppercase(value))
-                        : (evenstr = evenstr + value , oddstr = oddstr + toUppercase(value));
-    })
-    
-    return [oddstr + evenstr];
-  };
+  let sp = s.split('');
+  let oddstr = '';
+  let evenstr = '';
+  sp.forEach(function(value,index,arr){
+     (index%2 == 0) ? (oddstr += value , evenstr += value.toUpperCase())
+                      : (evenstr += value , oddstr += value.toUpperCase());
+  })
+  
+  return [`${evenstr}`, `${oddstr}`];
+};
